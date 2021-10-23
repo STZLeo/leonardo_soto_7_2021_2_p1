753facd6ce06495bfb887aeb2c5796dab16b8430
STZLeo <86271813+STZLeo@users.noreply.github.com>
2021-10-23 18:09:03 -0500

    Printed JSON Format on console
    Printed JSON Format on console

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,51 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:io';
 
-void main() => runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
+    return MaterialApp(home: DataFromApi());
+  }
+}
+
+class DataFromApi extends StatefulWidget {
+  @override
+  _DataFromApiState createState() => _DataFromApiState();
+}
+
+class _DataFromApiState extends State<DataFromApi> {
+  Future getMemeData() async {
+    var response =
+        await http.get(Uri.parse('https://api.doge-meme.lol/v1/memes/'));
+    Map data = jsonDecode(response.body);
+    print(data);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          title: Text('Memes'),
         ),
         body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
-    );
+            child: ElevatedButton(
+          child: Text('Click me'),
+          onPressed: () {
+            getMemeData();
+          },
+        )));
   }
+}
+
+class Memes {
+  final String submission_url, author, created;
+
+  Memes(this.submission_url, this.author, this.created);
 }
