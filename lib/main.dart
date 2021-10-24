f012cae436464c23bb2bd832c6824792db76a5b4
STZLeo <86271813+STZLeo@users.noreply.github.com>
2021-10-24 09:15:45 -0500

    Change the approach
    Decided to remove the code and set a MAP and convert it to a List to be able to "iterate" and then show the list as it should.
    
    Also added the images and important information about every entry.

--- lib/main.dart
+++ lib/main.dart
@@ -3,43 +3,35 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    ));
 
-class MyApp extends StatelessWidget {
+class HomePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: DataFromApi());
-  }
+  _HomePageState createState() => _HomePageState();
 }
 
-class DataFromApi extends StatefulWidget {
-  @override
-  _DataFromApiState createState() => _DataFromApiState();
-}
+class _HomePageState extends State<HomePage> {
+  late Map mapResponse;
+  late List listOfData;
 
-class _DataFromApiState extends State<DataFromApi> {
-  Future getMemeData() async {
-    var response =
-        await http.get(Uri.parse('https://api.doge-meme.lol/v1/memes/'));
-
-    Map jsonData = jsonDecode(response.body);
+  Future fetchData() async {
+    http.Response response;
+    response = await http.get(Uri.parse('https://api.doge-meme.lol/v1/memes/'));
     if (response.statusCode == 200) {
-      // If the server did return a 200 OK response,
-      // then parse the JSON.
-      //print(jsonData);
-      for (var m in jsonData.keys) {
-        var first = jsonData[m];
-        print(first);
-      }
-
-      return jsonDecode(response.body);
-    } else {
-      // If the server did not return a 200 OK response,
-      // then throw an exception.
-      throw Exception('Failed to load album');
+      setState(() {
+        mapResponse = json.decode(response.body);
+        listOfData = mapResponse['data'];
+      });
     }
+  }
+
+  @override
+  void initState() {
+    fetchData();
+    super.initState();
   }
 
   @override
@@ -48,18 +40,57 @@ class _DataFromApiState extends State<DataFromApi> {
         appBar: AppBar(
           title: Text('Memes'),
         ),
-        body: Center(
-            child: ElevatedButton(
-          child: Text('Get data'),
-          onPressed: () {
-            getMemeData();
-          },
-        )));
+        body: mapResponse == null
+            ? Container()
+            : SingleChildScrollView(
+                child: Column(
+                children: <Widget>[
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: EdgeInsets.all(15),
+                        child: Column(
+                          children: <Widget>[
+                            Image.network(listOfData[index]['submission_url']),
+/*                             Text(
+                              listOfData[index]['submission_id'].toString(),
+                              style: TextStyle(
+                                  fontSize: 30, fontWeight: FontWeight.bold),
+                            ), */
+                            Text(
+                              listOfData[index]['submission_url'].toString(),
+                              style: TextStyle(
+                                  fontSize: 30, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              listOfData[index]['submission_title'].toString(),
+                              style: TextStyle(
+                                  fontSize: 30, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              listOfData[index]['permalink'].toString(),
+                              style: TextStyle(
+                                  fontSize: 30, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              listOfData[index]['created'].toString(),
+                              style: TextStyle(
+                                  fontSize: 30, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              listOfData[index]['author'].toString(),
+                              style: TextStyle(
+                                  fontSize: 30, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                    itemCount: listOfData == null ? 0 : listOfData.length,
+                  )
+                ],
+              )));
   }
-}
-
-class Memes {
-  final List data;
-
-  Memes(this.data);
 }
